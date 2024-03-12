e13c6ba082c72c644466292a38d53192171831b3
nahomcode <157229307+nahomcode@users.noreply.github.com>
2024-03-12 14:04:26 +0300

    js update

---
+++ index.js
@@ -0,0 +1,31 @@
+function asmara(a, b) {
+  return a + b;
+}
+
+console.log("check:", asmara(2, 3));
+
+/*let h2element = document.querySelector("h2");
+h2element.innerText = hola;
+console.log(h2element.innerText);
+
+/*let h1element = document.querySelector("h1");
+console.log(h1element);
+*/
+function multiply(a, b) {
+  return a - b;
+}
+
+/*console.log("response:", multiply(7, 9));
+document.querySelector("html").addEventListener("click", () => {
+  alert("ouch dont touch me");
+});
+*/
+let person = {
+  name: "nahom",
+  age: 30,
+};
+console.log(person);
+
+const btn = document.createElement("button");
+btn.innerText = "find me";
+document.body.appendChild(btn);
